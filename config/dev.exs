674034afd9ca14bd7d558287fbb825d252748a06
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2026-06-08 09:18:56 +0300

    Fixed dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :sheglow, Sheglow.Repo,
 # Binding to loopback ipv4 address prevents access from other machines.
 config :sheglow, SheglowWeb.Endpoint,
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [port: 7151],
+  http: [port: 7451],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
